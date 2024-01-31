44a0a1c0868efd93c88c7d785cd33a59cc65a480
Angiii12 <angi.mari.rincon@gmail.com>
2024-01-31 11:28:40 -0300

    Creacion clase Objeto

--- Object.cpp
+++ Object.cpp
@@ -1,6 +1,20 @@
-#include "Object.h"
+#include "object.h"
 
-Object::Object() {
+Object::Object(std::string fname){
+	m_texture.loadFromFile(fname);
+	m_sprite.setTexture(m_texture);
+}
+
+void Object::Draw(RenderWindow &window){
+	window.draw(m_sprite);
+}
+
+bool Object::CollideWith(const Object &o) const{
+	auto r1 = this->m_sprite.getGlobalBounds();
+	auto r2 = o.m_sprite.getGlobalBounds();
+	return r1.intersects(r2);
 	
 }
 
+//getGlobalBounds (te da un rectangulito que representa donde está tu sprite)
+//intersects (metodo para ver si se intersectan los dos recangulitos)
--- Object.h
+++ Object.h
@@ -1,11 +1,18 @@
 #ifndef OBJECT_H
 #define OBJECT_H
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <string>
+#include <SFML/Graphics/RenderWindow.hpp>
+using namespace sf;
 
-class Object {
+class Object{
 public:
-	Object();
-private:
+	Object(std::string fname);
+	void Draw(RenderWindow &window);
+	bool CollideWith(const Object &o) const;
+protected:
+	Texture m_texture;
+	Sprite m_sprite;
 };
-
 #endif
-
